a1d6c038f6b6d0311b9fde8a8b40f7719cfbb180
gentom <gento.m.developer@gmail.com>
2017-10-29 22:07:21 +0900

    iDeea Engine

--- engine.py
+++ engine.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import sys
 from gensim.models import word2vec
 
-# Word2Vec
-# return words
-
-# call the model
-model = word2vec.Word2Vec.load('wiki_jpn') # there is no model yet.
+model = word2vec.Word2Vec.load('./model/wiki_jpn')
 
 def iDeea(word):
-    model.most_similar(word)
-    return model[word]
+    return model.most_similar([word])
+
+if __name__ == '__main__':
+    words = iDeea('日本')
+    print(words)
