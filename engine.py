957904093fd2c17ab6c3ca1d87aa63df427c5d89
gentom <gento.m.developer@gmail.com>
2017-10-29 21:09:18 +0900

    iDeea Engine

--- engine.py
+++ engine.py
@@ -5,3 +5,10 @@ from gensim.models import word2vec
 
 # Word2Vec
 # return words
+
+# call the model
+model = word2vec.Word2Vec.load('wiki_jpn') # there is no model yet.
+
+def ideea(word):
+    model.most_similar(word)
+    return model[word]
