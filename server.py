f8fc07ab79075e157b3d57b6e6f4a113f86e7972
gentom <gento.m.developer@gmail.com>
2017-10-30 17:44:34 +0900

    server

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from flask import Flask, request, jsonify
 import socket
+import urllib
 import json
 import engine
+import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind('', 8888)
+app = Flask(__name__)
 
-print('listening on port 8888')
+@app.route("/", methods=['POST'])
+def iDeeaServer():
+    if request.method == "POST":
+        word = request.args.get("word")
+        wordList = engine.iDeea(word)
+        #wordList_json = json.dumps(wordList, ensure_ascii=False)
+        #return jsonify(wordList_json)
+        return wordList
 
-while True:
-    data, addr = s.recvfrom(4096)
-
-    if data:
-        for i in range(0, len(data)):
-            print(data[i])
-        print (data, addr)
-        s.sendto(data, addr)
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
