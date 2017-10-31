690f86294e6851ef2ade72936b51c928aa99fa03
gentom <gento.m.developer@gmail.com>
2017-10-31 19:17:39 +0900

    Code Refactoring

--- server.py
+++ server.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
-import socket
-import urllib
 import json
 import engine
-import sys
 
 app = Flask(__name__)
-
-wordList = []
 
 @app.route("/", methods=['POST'])
 def iDeeaServer():
@@ -20,7 +15,6 @@ def iDeeaServer():
         print(wordList)
         wordList_json = json.dumps(wordList, ensure_ascii=False)
         return wordList_json
-        #return word
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
