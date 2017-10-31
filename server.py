77201f1451d5c61b4dd07d7c8558a14e0258b29b
gentom <gento.m.developer@gmail.com>
2017-10-31 18:08:58 +0900

    add comment

--- server.py
+++ server.py
@@ -14,14 +14,13 @@ wordList = []
 @app.route("/", methods=['POST'])
 def iDeeaServer():
     if request.method == "POST":
-        #word = request.args.get("word")
         word = request.form['word']
         print(word)
         wordList = engine.iDeea(word)
         print(wordList)
         #wordList_json = json.dumps(wordList, ensure_ascii=False)
         #return jsonify(wordList_json)
-        return '200'
+        return '200' # responseCode to unity client
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
