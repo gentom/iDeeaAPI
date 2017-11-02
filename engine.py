2f0bce4b0ff0201dfa1119f57e04c59ae1fae77f
gentom <gento.m.developer@gmail.com>
2017-11-02 13:41:02 +0900

    remove

--- engine.py
+++ engine.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+from flask import Flask, request, jsonify, session
+import json
+from flask_socketio import SocketIO
 from gensim.models import word2vec
+import sys
 
-model = word2vec.Word2Vec.load('./model/wiki_jpn')
+async_mode = None
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode = async_mode)
+thread = None
 
-def iDeea(word):
-    words = model.most_similar([word])
-    wordList = [words[i][0] for i in range(len(words))]
-    return wordList
+model = word2vec.Word2Vec.load('./model/jp_wiki')
 
-# test
-if __name__ == '__main__':
-    print(iDeea('サッカー'))
+@app.route("/", methods=['POST'])
+def iDeeaServer():
+    if request.method == "POST":
+        word = request.form['word']
+        print(word)
+        words = model.most_similar([word])
+        wordList = [words[i][0] for i in range(len(words))]
+        print(wordList)
+        wordList_json = json.dumps(wordList, ensure_ascii=False)
+        return wordList_json
+
+@app.route("/merge", methods=['POST'])
+def MergeX():
+    if request.method == "POST":
+        word = request.form['word']
+        # mecabで分かち書き
+        # 二つの単語に分割
+        # Word2Vecにて二つの単語の加算
+
+if __name__ == "__main__":
+    #app.run(debug=True, port=8000)
+    socketio.run(app, debug=True, port=8000)
